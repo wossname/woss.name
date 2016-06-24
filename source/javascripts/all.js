d5496a84f0d3ae371a1d13e34999bd84cb4686ce
Graeme Mathieson <mathie@woss.name>
2016-06-24 15:09:07 +0100

    Show times as relative, with tooltips.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,14 @@
 // This is where it all goes :)
+
+$(function() {
+  $('time').each(function() {
+    var time = $(this).attr('datetime');
+    var theMoment = moment(time);
+    $(this).attr('title', $(this).html());
+    $(this).data('placement', 'top');
+    $(this).tooltip();
+    $(this).html(theMoment.fromNow());
+  });
+
+  $('abbr').tooltip();
+});
