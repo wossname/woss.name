bbbb7d38296fc608a371885b77b60b1f3983fbd0
Graeme Mathieson <mathie@woss.name>
2016-06-24 14:24:33 +0100

    Change published/updated to 'on' to reflect that they're dates.

--- helpers/metadata_helper.rb
+++ helpers/metadata_helper.rb
@@ -15,12 +15,29 @@ module MetadataHelper
     (current_page.data.tags || config[:default_tags] || []) + (config[:site_tags] || [])
   end
 
-  def published_at_meta
-    current_page.data.published_at || Time.now
+  def published_on_meta
+    if (published_on = current_page.data.published_on)
+      published_on.acts_like?(:date) ? published_on : Date.parse(published_on)
+    else
+      # FIXME: This should pull the last modified time from Git/the filesystem.
+      Date.today
+    end
   end
 
-  def updated_at_meta
-    current_page.data.updated_at || published_at_meta
+  def published_on_tag
+    time_tag published_on_meta
+  end
+
+  def updated_on_meta
+    if (updated_on = current_page.data.updated_on)
+      Date.parse(updated_on)
+    else
+      published_on_meta
+    end
+  end
+
+  def updated_on_tag
+    time_tag updated_on_meta
   end
 
   def url_meta
